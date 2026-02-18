f1fa976397c1428e2724484c260d0da72bf494ed
Sach-123 <sachinchoudhary0203@gmail.com>
2026-02-18 17:10:25 +0530

    transactional testing

---
+++ hospitalManagement/src/main/java/com/example/hospitalManagement/service/PatientService.java
@@ -0,0 +1,31 @@
+package com.example.hospitalManagement.service;
+
+import com.example.hospitalManagement.entity.Patient;
+import com.example.hospitalManagement.repository.PatientRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PatientService {
+    private final PatientRepository patientRepository;
+
+    @Transactional
+    public Patient getPatient(Long id){
+        Patient p1 = patientRepository.findById(id).orElseThrow();
+
+        Patient p2 = patientRepository.findById(id).orElseThrow();
+
+        System.out.println(p1==p2);
+        return p1;
+    }
+
+
+
+
+
+
+
+
+}
--- hospitalManagement/src/test/java/com/example/hospitalManagement/PatientTest.java
+++ hospitalManagement/src/test/java/com/example/hospitalManagement/PatientTest.java
@@ -2,6 +2,7 @@ package com.example.hospitalManagement;
 
 import com.example.hospitalManagement.entity.Patient;
 import com.example.hospitalManagement.repository.PatientRepository;
+import com.example.hospitalManagement.service.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,10 +15,18 @@ public class PatientTest {
     @Autowired
     private PatientRepository patientRepository;
 
+    @Autowired
+    private PatientService patientService;
+
     @Test
     public void testPatientRepository(){
 
         List<Patient> pist = patientRepository.findAll();
         System.out.println(pist);
+    }
+
+    @Test
+    public void testingPatient(){
+        Patient patient = patientService.getPatient(1L);
     }
 }
